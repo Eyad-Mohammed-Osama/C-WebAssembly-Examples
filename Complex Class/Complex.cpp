eea8a7c60c782b4809a84fba5286801844ab4b48
EyadMohammedOsama <eyadbere@gmail.com>
2020-06-07 02:42:42 +0300

    Added ToString() method

--- Complex Class/Complex.cpp
+++ Complex Class/Complex.cpp
@@ -5,23 +5,23 @@ using namespace emscripten;
 
 static int count = 0;
 
-class Complex { 
-    private:
+class Complex {
+private:
     double real;
     double imaginary;
 
-    public:
+public:
     Complex(double real = 0, double imaginary = 0) {
         this->real = real;
         this->imaginary = imaginary;
-        count++;    //  Each instance increase the counter by 1
+        count++; //  Each instance increase the counter by 1
     }
 
     Complex Add(Complex c) {
         Complex result = Complex();
         result.real = this->real + c.real;
         result.imaginary = this->imaginary + c.imaginary;
-        count--;    //  When creating result object , a false increment happens
+        count--; //  When creating result object , a false increment happens
         return result;
     }
 
@@ -29,7 +29,7 @@ class Complex {
         Complex result = Complex();
         result.real = this->real - c.real;
         result.imaginary = this->imaginary - c.imaginary;
-        count--;    //  When creating result object , a false increment happens
+        count--; //  When creating result object , a false increment happens
         return result;
     }
 
@@ -53,22 +53,42 @@ class Complex {
         return this->imaginary;
     }
 
+    std::string ToString() const {
+        std::string str = "";
+        str += std::to_string(this->real);
+        if (this->imaginary != 0) {
+            if (this->imaginary > 0) {
+                str += " + ";
+            }
+            else {
+                str += " - ";
+            }
+            str += "(";
+            str += std::to_string(this->imaginary);
+            str += ")i";
+        }
+        return str;
+    }
+
     ~Complex() {
-        count--;    //  Decrease the counter if an object destructor has been called
+        count--; //  Decrease the counter if an object destructor has been called
     }
 };
 
-EMSCRIPTEN_BINDINGS(module) {
-    class_<Complex>("Complex")                  //  Exporting the class to JS as "Complex"
-    .constructor<double, double>()              //  Exporting the constructor , it takes 2 parameters of type double
-    .function("Add", &Complex::Add)             //  Exporting the "Add()" method from Complex
-    .function("Subtract", &Complex::Subtract)   //  Exporting the "Subtract()" method from Complex
-    .property("Length", &Complex::Length)       //  Exporting the "Length()" method from Complex as a read-only property
-    .property("Real", &Complex::GetReal, &Complex::SetReal)                 //  Exporting the "GetReal() / SetReal()" methods as a read/write property
-    .property("Imaginary", &Complex::GetImaginary, &Complex::SetImaginary)  //  Exporting the "GetImaginary() / SetImaginary()" methods as a read/write property
-    .class_property("Count", &count);           //  Exporting the static counter as a read-only property
+EMSCRIPTEN_BINDINGS(module)
+{
+    class_<Complex>("Complex")                                                 //  Exporting the class to JS as "Complex"
+        .constructor<double, double>()                                         //  Exporting the constructor , it takes 2 parameters of type double
+        .function("Add", &Complex::Add)                                        //  Exporting the "Add()" method from Complex
+        .function("Subtract", &Complex::Subtract)                              //  Exporting the "Subtract()" method from Complex
+        .function("ToString", &Complex::ToString)                              //  Exporting the "ToString()" method from Complex
+        .property("Length", &Complex::Length)                                  //  Exporting the "Length()" method from Complex as a read-only property
+        .property("Real", &Complex::GetReal, &Complex::SetReal)                //  Exporting the "GetReal() / SetReal()" methods as a read/write property
+        .property("Imaginary", &Complex::GetImaginary, &Complex::SetImaginary) //  Exporting the "GetImaginary() / SetImaginary()" methods as a read/write property
+        .class_property("Count", &count);                                      //  Exporting the static counter as a read-only property
 }
 
+// You can comment out main() function
 int main() {
     std::cout << "Complex module has been loaded successfully" << std::endl;
     return 0;
